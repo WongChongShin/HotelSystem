81d26f14e07670991afde69078e277bb52af0c3b
LZY000126 <73522418+LZY000126@users.noreply.github.com>
2021-03-06 23:43:50 +0800

    -checkIn&checkOut list havent done but add and edit done -facility add and edit for pool done

---
+++ app/src/main/java/com/example/mobilehotelmanagementsystem/EditPoolList.kt
@@ -0,0 +1,11 @@
+package com.example.mobilehotelmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPoolList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_pool_list)
+    }
+}
