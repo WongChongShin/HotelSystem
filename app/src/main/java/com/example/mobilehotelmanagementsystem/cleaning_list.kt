ab86bf836f98ab0b2be5bf1d37562cbcb2453c8d
WongChongShin <73522463+WongChongShin@users.noreply.github.com>
2021-03-15 14:41:10 +0800

    little things

---
+++ app/src/main/java/com/example/mobilehotelmanagementsystem/cleaning_list.kt
@@ -0,0 +1,26 @@
+package com.example.mobilehotelmanagementsystem
+
+import android.database.DatabaseUtils
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class cleaning_list : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //val v = inflater.inflate(R.layout.)
+
+        /*val binding = .inflate<cleaning_list>(inflater,
+            R.layout.fragment_cleaning_list,container,false)
+        return binding.root*/
+        return null
+    }
+
+}
---
+++ app/src/main/java/com/example/mobilehotelmanagementsystem/facilities_monitoring.kt
@@ -0,0 +1,11 @@
+package com.example.mobilehotelmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class facilities_monitoring : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facilities_monitoring)
+    }
+}
